d8b50c4a05c449c4098181483ba5413e6d1bb114
Richard Carruthers <richardcarruthers8@gmail.com>
2025-02-16 13:23:41 +0000

    basic game loop

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
 import chalk from "chalk";
+import readlineSync from "readline-sync";
 
-console.log(chalk.blue("Hello world!"));
+console.log(
+  chalk.greenBright("Game intro"),
+  chalk.green("(type quit to exit)")
+);
+console.log();
+
+while (true) {
+  const line = readlineSync.question(" > ").trim();
+  console.log();
+
+  if (line === "quit") {
+    break;
+  }
+
+  console.log(chalk.green("Game response\n"));
+}
